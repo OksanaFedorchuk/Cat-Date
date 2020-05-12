26cf90e4a2f8550931aee010b921c9fb5be180da
Oksana <klassi4eske@gmail.com>
2020-05-12 15:01:45 +0300

    2. VC2 setup

---
+++ Cat Date/View Controllers/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  Cat Date
+//
+//  Created by Oksana Fedorchuk on 12.05.2020.
+//  Copyright © 2020 Oksana Fedorchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    @IBOutlet weak var matchLabel: UILabel!
+    @IBOutlet weak var matchCatImage: UIImageView!
+    @IBOutlet weak var temperamentLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var wikiLabel: UILabel!
+    @IBOutlet weak var againButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func againButtonTapped(_ sender: Any) {
+    }
+    
+    
+}
